aabe41a1c00962c2f8fed6b51844495112e91557
Majid Jalili <jalili2006@gmail.com>
2025-07-17 10:15:26 +0200

    Update netadapter_up.ps1

--- netadapter_up.ps1
+++ netadapter_up.ps1
@@ -5,3 +5,24 @@ Get-NetAdapter| Where-Object {$_.status -eq 'Up'} | Select-Object Name, Status, 
 
 Get-NetAdapter | Select-Object ifIndex,Name
 
+# Show only Ethernet adapters that are up, with key properties
+Get-NetAdapter |
+    Where-Object { $_.Status -eq 'Up' -and $_.InterfaceDescription -like '*Ethernet*' } |
+    Select-Object ifIndex, Name, Status, MacAddress, LinkSpeed
+
+
+# Show only Ethernet adapters that are up, with key properties and IP summary
+$adapters = Get-NetAdapter |
+    Where-Object { $_.Status -eq 'Up' -and $_.InterfaceDescription -like '*Ethernet*' }
+
+foreach ($adapter in $adapters) {
+    $info = [PSCustomObject]@{
+        ifIndex    = $adapter.ifIndex
+        Name       = $adapter.Name
+        Status     = $adapter.Status
+        MacAddress = $adapter.MacAddress
+        LinkSpeed  = $adapter.LinkSpeed
+        IPs        = (Get-NetIPAddress -InterfaceIndex $adapter.ifIndex | Select-Object -ExpandProperty IPAddress)
+    }
+    $info
+}
